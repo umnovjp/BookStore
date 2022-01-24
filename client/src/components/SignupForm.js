5c5b374b4bac770deba3dfa9fa2ed1c48041dbcd
umnovjp <umnovjp@yahoo.com>
2022-01-23 16:28:23 -0800

    signup is not working to ask BCS

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,13 +1,16 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
+import { ADD_USER } from '../utils/mutations';
+import { useMutation } from '@apollo/client';
 
-import { createUser } from '../utils/API';
+// import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
   // set initial form state
   const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
   // set state for form validation
+  const [addUser, { error }] = useMutation(ADD_USER);
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
@@ -28,15 +31,18 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+     // const response = await addUser(userFormData);
+     const { data } = await addUser({
+      variables: { ...userFormData },
+    });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      // if (!response.ok) {
+      //   throw new Error('something went wrong!');
+      // }
 
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      // const { token, user } = await response.json();
+      // console.log(user);
+      Auth.login(data.addUser.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
